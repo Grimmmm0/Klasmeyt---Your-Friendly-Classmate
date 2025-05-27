446061fa6aa50062a837048773c4b24c8fcef08a
Grimmmm0 <boneclinkz35@gmail.com>
2025-05-27 16:41:32 +0800

    Gemini fix, llm fix back end almost compelte front end and back end connection not yet done

--- server/services/llm_service.py
+++ server/services/llm_service.py
@@ -1,7 +1,30 @@
 import google.generativeai as genai
+from config import Settings
+
+settings = Settings()
 class LLMService:
     def __init__(self):
-        self.model = genai.GenerativeModel("")
+        genai.configure(api_key=settings.GEMINI_API_KEY)
+        self.model = genai.GenerativeModel("gemini-2.0-flash")
     
     def generate_response(self, query: str, search_results: list[dict]):
-        pass
+         context_text = "\n\n".join([
+             f"Source {i+1} ({result['url']}):\n{result['content']}"
+            for i, result in enumerate(search_results)
+         ])
+         
+         full_prompt = f"""
+         Ayon saaking research Klasmeyt, ang tanong mo ay:
+         {context_text}
+         
+         Query: {query}
+         
+         Ang Labo mo Klasmeyt, ayusin mo tanong mo lods, dapat malinaw at maayos.
+         """
+        
+         response = self.model.generate_content(full_prompt, stream=True)
+         
+         for chunk in response:
+             yield chunk.text
+             
+         
