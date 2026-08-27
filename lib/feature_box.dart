85bddaf69bdfdaea59088b46da2b06a6842639b0
Grimmmm0 <boneclinkz35@gmail.com>
2026-08-28 01:05:59 +0800

    Working on assets and UI

---
+++ lib/feature_box.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class FeatureBox extends StatelessWidget {
+  final Color color;
+  final String headerText;
+  final String descriptionText;
+  const FeatureBox(
+      {super.key,
+      required this.color,
+      required this.headerText,
+      required this.descriptionText});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(
+        horizontal: 35,
+        vertical: 10,
+      ),
+      decoration: BoxDecoration(
+          color: color,
+          borderRadius: const BorderRadius.all(Radius.circular(15))),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 20).copyWith(left: 15),
+        child: Column(
+          children: [
+            Text(
+              headerText,
+              style: const TextStyle(
+                  color: Colors.black,
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold),
+            ),
+            Text(
+              descriptionText,
+              style: const TextStyle(color: Colors.black, fontSize: 14),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:klasmeyt/classes/message.dart';
+import 'package:klasmeyt/feature_box.dart';
+import 'package:klasmeyt/themes/colors.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -9,18 +10,86 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  final List<Message> _messages = [
-    Message(text: "Yow", isUser: true),
-    Message(text: "Yow, Ano meron?", isUser: false),
-    Message(text: "Ayus lang, ikaw?", isUser: true),
-    Message(text: "Angas", isUser: false),
-  ];
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Center(
-      child: Text("tang ina mo"),
-    ));
+      appBar: AppBar(
+        title: const Text('Klasmeyt'),
+        centerTitle: true,
+        leading: const Icon(Icons.menu_outlined),
+        backgroundColor: AppColors.iconGrey,
+      ),
+      body: Column(
+        children: [
+          Stack(
+            children: [
+              Center(
+                child: Container(
+                  height: 115,
+                  width: 100,
+                  margin: const EdgeInsets.only(top: 4),
+                  decoration: const BoxDecoration(
+                    color: AppColors.proButton,
+                    shape: BoxShape.circle,
+                  ),
+                ),
+              ),
+              Container(
+                height: 123,
+                decoration: const BoxDecoration(
+                  shape: BoxShape.circle,
+                  image:
+                      DecorationImage(image: AssetImage('assets/student.png')),
+                ),
+              )
+            ],
+          ),
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            margin: const EdgeInsets.symmetric(
+              horizontal: 40,
+            ).copyWith(top: 30),
+            decoration: BoxDecoration(
+              border: Border.all(
+                color: AppColors.textGrey,
+              ),
+              borderRadius: BorderRadius.circular(20).copyWith(
+                topLeft: Radius.zero,
+              ),
+            ),
+            child: const Padding(
+              padding: EdgeInsets.symmetric(vertical: 10),
+              child: Text(
+                'Gandang Araw, Klasmeyt ano yun?',
+                style: TextStyle(color: AppColors.whiteColor, fontSize: 25),
+              ),
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.all(10),
+            alignment: Alignment.centerLeft,
+            margin: const EdgeInsets.only(
+              top: 10,
+              left: 22,
+            ),
+            child: const Text('Ito ang aking mga tampok',
+                style: TextStyle(
+                    color: AppColors.whiteColor,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold)),
+          ),
+          const Column(
+            children: [
+              FeatureBox(
+                color: AppColors.submitButton,
+                headerText: 'ChatGPT',
+                descriptionText:
+                    'AI nang nakakarami ang ChatGPT kahit lola mo alam',
+              )
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
